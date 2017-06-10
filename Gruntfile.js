1d683661fd679853301eff1ce629d03c0160870d
Mo Bitar <me@bitar.io>
2017-06-10 18:59:45 -0500

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    watch: {
+      js: {
+        files: ['.src/**/*.js'],
+        tasks: ['babel', 'browserify'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+
+   babel: {
+        options: {
+            sourceMap: true,
+            presets: ['es2016']
+        },
+        dist: {
+            files: {
+                'dist/dist.js': 'src/componentManager.js'
+            }
+        }
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          'dist/dist.js': 'dist/dist.js'
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('default', ['babel', 'browserify']);
+};

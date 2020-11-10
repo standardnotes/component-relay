8f504d23ef6034a7efa85020cf0fd52211ff60d2
Johnny Almonte <johnny243@users.noreply.github.com>
2020-11-09 23:43:31 -0400

    test: logger

---
+++ test/logger.test.js
@@ -0,0 +1,38 @@
+import Logger from './../lib/logger';
+
+describe("Logger", () => {
+  beforeAll(() => {
+    console.log = jest.fn();
+    console.error = jest.fn();
+  });
+
+  it('should output messages to console anything by default', () => {
+    Logger.info('A simple message.');
+    expect(Logger.enabled).toBe(false);
+    expect(console.log).not.toBeCalled();
+  });
+
+  it('should output messages to console if "enabled" is true', () => {
+    Logger.enabled = true;
+    Logger.info('A simple message.');
+    expect(Logger.enabled).toBe(true);
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(['A simple message.']);
+  });
+
+  it('should output errors to console if "enabled" is false', () => {
+    Logger.enabled = false;
+    Logger.error('An error occured.');
+    expect(Logger.enabled).toBe(false);
+    expect(console.error).toBeCalledTimes(1);
+    expect(console.error).toBeCalledWith(['An error occured.']);
+  });
+
+  it('should output errors to console if "enabled" is true', () => {
+    Logger.enabled = true;
+    Logger.error('An error occured.');
+    expect(Logger.enabled).toBe(true);
+    expect(console.error).toBeCalledTimes(1);
+    expect(console.error).toBeCalledWith(['An error occured.']);
+  });
+});

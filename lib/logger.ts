dc7c7c301c592a5b17a7725478948223a19c84ba
Johnny Almonte <johnny243@users.noreply.github.com>
2020-11-10 00:22:04 -0400

    fix: remove explicit-module-boundary-types from logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default class Logger {
   public static enabled = false;

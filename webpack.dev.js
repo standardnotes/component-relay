ab4ba7025e06a4289469d6e2c5ef07f362cc1b00
Johnny Almonte <johnny243@users.noreply.github.com>
2020-11-12 22:41:08 -0400

    feat: remove dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,35 +1,16 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const config = require('./webpack.config.js');
-const resolveAppPath = relativePath => path.resolve(__dirname, relativePath);
 
 module.exports = merge(config, {
   mode: 'development',
-  devServer: {
-    contentBase: resolveAppPath('ext'),
-    publicPath: '/',
-    compress: true,
-    hot: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    },
-    port: 8081,
-  },
   output: {
-    filename: 'dev.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'dist.js',
+    sourceMapFilename: 'dist.js.map',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: resolveAppPath('ext/index.html'),
-    }),
-  ],
   devtool: 'eval-cheap-module-source-map',
   stats: {
-    colors: true,
-  },
+    colors: true
+  }
 });

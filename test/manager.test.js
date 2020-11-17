5489d9a30d06a820c7197c04956cf5a91655a092
Johnny Almonte <johnny243@users.noreply.github.com>
2020-11-17 01:09:03 -0400

    wip: componentManager tests

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,47 +1,77 @@
 import ComponentManager from './../lib/componentManager';
 
+const postMessage = async (message, targetOrigin) => {
+  window.postMessage(message, targetOrigin);
+
+  /**
+   * window.postMesasge() implementation is wrapped with setTimeout.
+   * See https://github.com/jsdom/jsdom/issues/2245#issuecomment-392556153
+   */
+  await new Promise(resolve => setTimeout(resolve, 10));
+};
+
+const componentRegisteredMessage = {
+  action: 'component-registered',
+  data: {
+    sessionKey: 'session-key',
+    componentData: {},
+    uuid: "component-uuid",
+    origin: "http://localhost",
+    data: {},
+    environment: "web",
+    platform: "linux",
+    isMobile: false,
+    acceptsThemes: true,
+    activeThemes: [],
+    activeThemeUrls: []
+  },
+  api: "component",
+};
+
+const registeredComponentAction = async () => {
+  await postMessage(componentRegisteredMessage, '*');
+};
+
 describe("ComponentManager", () => {
-  test('initialization without parameters', () => {
-    const componentManager = new ComponentManager();
+  let componentManager;
+  let onReady;
+
+  beforeEach(() => {
+    onReady = jest.fn();
+    componentManager = new ComponentManager({
+      onReady,
+    });
+  });
+
+  afterEach(() => {
+    componentManager = undefined;
+  });
+
+  it('should not be undefined', () => {
     expect(componentManager).not.toBeUndefined();
   });
 
-  test('initialization with onReady parameter', async function () {
-    const onReadyCallback = jest.fn();
+  it('should not run onReady callback when component has not been registered', async () => {
+    expect(onReady).toBeCalledTimes(0);
+  });
+
+  it('should run onReady callback when component is registered', async () => {
+    await registeredComponentAction();
+    expect(onReady).toBeCalledTimes(1);
+  });
+
+  test('getSelfComponentUUID() before the component is registered should be undefined', async () => {
     const componentManager = new ComponentManager({
-      onReady: onReadyCallback,
-      options: {}
+      onReady,
     });
+    const uuid = componentManager.getSelfComponentUUID();
+    expect(uuid).toBeUndefined();
+  });
 
-    const messageData = {
-      action: 'component-registered',
-      data: {
-        sessionKey: 'session-key',
-        componentData: {},
-        uuid: "component-uuid",
-        origin: "http://localhost",
-        data: {},
-        environment: "web",
-        platform: "linux",
-        isMobile: false,
-        acceptsThemes: true,
-        activeThemes: [],
-        activeThemeUrls: []
-      },
-      componentData: {},
-      messageId: "",
-      sessionKey: "",
-      api: "",
-      original: "",
-    };
-    window.postMessage(messageData, '*');
-
-    /**
-     * window.postMesasge() implementation is wrapped with setTimeout.
-     * See https://github.com/jsdom/jsdom/issues/2245#issuecomment-392556153
-     */
-    await new Promise(resolve => setTimeout(resolve, 10));
-
-    expect(onReadyCallback).toBeCalledTimes(1);
+  test('getSelfComponentUUID() after the component is registered should not be undefined', async () => {
+    await registeredComponentAction();
+    const uuid = componentManager.getSelfComponentUUID();
+    expect(uuid).not.toBeUndefined();
+    expect(uuid).toBe(componentRegisteredMessage.data.uuid);
   });
 });

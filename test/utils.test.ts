66d85a3933c50e55010a28ffbc1f32da45429891
Johnny Almonte <johnny243@users.noreply.github.com>
2020-12-09 00:42:28 -0400

    feat: tests for generateUuid

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,6 +1,34 @@
+// @ts-nocheck
 import { isValidJsonString } from './../lib/utils';
+import crypto from 'crypto';
+
+const uuidFormat = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 
 describe("Utils", () => {
+  describe('generateUuid', () => {
+    // Use the real implementation of generateUuid instead of the mocked one.
+    const { generateUuid } = jest.requireActual('./../lib/utils');
+
+    test("length should be 36 characters", () => {
+      const uuid = generateUuid();
+      expect(uuid.length).toEqual(36);
+    });
+
+    it("should have a valid format", () => {
+      const uuid = generateUuid();
+      expect(uuid).toEqual(expect.stringMatching(uuidFormat));
+    });
+
+    test("uuid generated using window.crypto should have the correct format and length", () => {
+      global.crypto = {
+        getRandomValues: (array) => crypto.randomBytes(array.length)
+      };
+      const uuid = generateUuid();
+      expect(uuid.length).toEqual(36);
+      expect(uuid).toEqual(expect.stringMatching(uuidFormat));
+    });
+  });
+
   describe('isValidJsonString', () => {
     test("anything other than string should return false", () => {
       let result = isValidJsonString(1);

c8e30b11cc6d59e96386675b0a00a9f6f08b4ec0
Johnny Almonte <johnny243@users.noreply.github.com>
2021-01-05 23:01:59 -0400

    chore(deps): update bundle and types

--- dist/@types/componentManager.d.ts
+++ dist/@types/componentManager.d.ts
@@ -2,12 +2,32 @@ import { ComponentAction, ComponentPermission, ContentType, SNItem, AppDataField
 declare type ComponentManagerOptions = {
     coallesedSaving?: boolean;
     coallesedSavingDelay?: number;
+    /**
+     * Outputs debugging information to console.
+     */
     debug?: boolean;
+    /**
+     * Indicates whether or not the component accepts themes.
+     */
     acceptsThemes?: boolean;
 };
 declare type ComponentManagerParams = {
+    /**
+     * Represents the window object that the component is running in.
+     */
+    targetWindow: Window;
+    /**
+     * A collection of permissions that the component can request
+     * access once it's ready.
+     */
     initialPermissions?: ComponentPermission[];
+    /**
+     * The options to initialize
+     */
     options?: ComponentManagerOptions;
+    /**
+     * A callback that is executed after the component has been registered.
+     */
     onReady?: () => void;
 };
 declare type ItemPayload = {
@@ -29,17 +49,39 @@ export default class ComponentManager {
     private coallesedSaving;
     private coallesedSavingDelay;
     private messageHandler?;
-    constructor(contentWindow: Window, params?: ComponentManagerParams);
+    constructor(params: ComponentManagerParams);
     private processParameters;
     deinit(): void;
     private registerMessageHandler;
     private handleMessage;
     private onReady;
+    /**
+     * Gets the components UUID.
+     */
     getSelfComponentUUID(): string | undefined;
+    /**
+     * Checks if the component is running in a Desktop application.
+     */
     isRunningInDesktopApplication(): boolean;
+    /**
+     * Checks if the component is running in a Mobile application.
+     */
     isRunningInMobileApplication(): boolean;
+    /**
+     * Gets the component's data value for the specified key.
+     * @param key The key for the data object.
+     * @returns `undefined` if the value for the key does not exist. Returns the stored value otherwise.
+     */
     getComponentDataValueForKey(key: string): any;
+    /**
+     * Sets the component's data value for the specified key.
+     * @param key The key for the data object.
+     * @param value The value to store under the specified key.
+     */
     setComponentDataValueForKey(key: string, value: any): void;
+    /**
+     * Clears the component's data object.
+     */
     clearComponentData(): void;
     private postMessage;
     private requestPermissions;
@@ -47,30 +89,71 @@ export default class ComponentManager {
     private themeElementForUrl;
     private deactivateTheme;
     private generateUUID;
+    /**
+     * Gets the current platform where the component is running.
+     */
     get platform(): string | undefined;
+    /**
+     * Gets the current environment where the component is running.
+     */
     get environment(): string | undefined;
-    /** Components actions */
+    /**
+     * Streams a collection of Items, filtered by content type.
+     * New items are passed to the callback as they come.
+     * @param contentTypes A collection of Content Types.
+     * @param callback A callback to process the streamed items.
+     */
     streamItems(contentTypes: ContentType[], callback: (data: any) => void): void;
+    /**
+     * Streams the current Item in context.
+     * @param callback A callback to process the streamed item.
+     */
     streamContextItem(callback: (data: any) => void): void;
     /**
-     * Selects an item which typically needs to be a tag.
-     * @param item the item to select.
+     * Selects an item which typically needs to be a Tag.
+     * @param item The Item to select.
      */
     selectItem(item: SNItem): void;
     /**
-     * Clears current selected tags.
+     * Clears current selected Tag (if any).
      */
     clearSelection(): void;
+    /**
+     * Creates and stores an Item in the item store.
+     * @param item The Item's payload content.
+     * @param callback The callback to process the created Item.
+     */
     createItem(item: ItemPayload, callback: (data: any) => void): void;
+    /**
+     * Creates and stores a collection of Items in the item store.
+     * @param items The Item(s) payload collection.
+     * @param callback The callback to process the created Item(s).
+     */
     createItems(items: ItemPayload[], callback: (data: any) => void): void;
     associateItem(item: ItemPayload): void;
     deassociateItem(item: ItemPayload): void;
+    /**
+     * Deletes an Item from the item store.
+     * @param item The Item to delete.
+     * @param callback The callback with the result of the operation.
+     */
     deleteItem(item: SNItem, callback: (data: any) => void): void;
+    /**
+     * Deletes a collection of Items from the item store.
+     * @param items The Item(s) to delete.
+     * @param callback The callback with the result of the operation.
+     */
     deleteItems(items: SNItem[], callback: (data: any) => void): void;
     sendCustomEvent(action: ComponentAction, data: any, callback?: (data: any) => void): void;
+    /**
+     * Saves an existing Item in the item store.
+     * @param item An existing Item to be saved.
+     * @param callback
+     * @param skipDebouncer
+     */
     saveItem(item: SNItem, callback?: () => void, skipDebouncer?: boolean): void;
     /**
-     * @param item The item to be saved.
+     * @param item An existing Item to be saved.
      * @param presave Allows clients to perform any actions last second before the save actually occurs (like setting previews).
      * Saves debounce by default, so if a client needs to compute a property on an item before saving, it's best to
      * hook into the debounce cycle so that clients don't have to implement their own debouncing.
@@ -78,7 +161,7 @@ export default class ComponentManager {
      */
     saveItemWithPresave(item: SNItem, presave: any, callback: () => void): void;
     /**
-     * @param items The items to be saved.
+     * @param items A collection of existing Items to be saved.
      * @param presave Allows clients to perform any actions last second before the save actually occurs (like setting previews).
      * Saves debounce by default, so if a client needs to compute a property on an item before saving, it's best to
      * hook into the debounce cycle so that clients don't have to implement their own debouncing.
@@ -94,8 +177,20 @@ export default class ComponentManager {
      * @param presave
      */
     saveItems(items: SNItem[], callback?: () => void, skipDebouncer?: boolean, presave?: any): void;
+    /**
+     * Sets a new size for the current component.
+     * @param type The new size type (container).
+     * @param width The new width.
+     * @param height The new height.
+     */
     setSize(type: string, width: string | number, height: string | number): void;
     private jsonObjectForItem;
+    /**
+     * Gets the Item's appData value for the specified key.
+     * Uses the default domain (org.standardnotes.sn).
+     * @param item The Item to get the appData value from.
+     * @param key The key to get the value from.
+     */
     getItemAppDataValue(item: SNItem, key: AppDataField): any;
 }
 export {};
